81e7b90ce6ec1dc6e1f509e27156d1a9da3a9fd9
Glyne J. Gittens <Jgit121@gmail.com>
2016-11-13 23:40:23 -0500

    Successfully finds Bowling scores. Time to account for edge cases and exceptions.

--- java/Bowling.java
+++ java/Bowling.java
@@ -3,18 +3,83 @@ import java.util.*;
 public class Bowling{
   public static void main(String[] args){
       List<Integer> scores = getScores(args);
-      for(int in : scores){
-          System.out.println(in);
-      }
       printCardHead();
-      printRow(1,2,3,4,5);
+      int       score = 0,
+                index = 0,
+                frame_sum = 0;
+
+      // frames
+      for(int i=1; i <= 10; i++){
+
+          if(scores.get(index) == 10){
+              score = score
+                      + scores.get(index)
+                      + scores.get(index + 1)
+                      + scores.get(index + 2);
+
+              // Handle wat happens when the you're on the last frame
+              if(i == 10){
+                  printRow(i,
+                            scores.get(index),
+                            scores.get(index+1),
+                            scores.get(index+2),
+                            score);
+
+              }else{
+                  printRow(i,
+                            scores.get(index),
+                            0,
+                            0,
+                            score);
+              }
+              index++;
+              continue;
+          }
+          // Handle Spare
+          else if((scores.get(index) + scores.get(index+1)) == 10 ){
+              score = score
+                      + 10
+                      + scores.get(index + 2);
+              if(i == 10){
+                  printRow(i,
+                          scores.get(index),
+                          scores.get(index+1),
+                          scores.get(index+2),
+                          score);
+              }else{
+                  printRow(i,
+                          scores.get(index),
+                          scores.get(index + 1),
+                          0,
+                          score);
+              }
+              index += 2;
+              continue;
+          }
+          //handle bad players
+          else{
+              score = score
+                      + scores.get(index )
+                      + scores.get(index + 1);
+              printRow(i,
+                        scores.get(index),
+                        scores.get(index+1),
+                        0,
+                        score);
+              index += 2;
+              continue;
+          }
+      }
   }
+
+
+
   static void printCardHead(){
       System.out.println("FR\tR1\tR2\tR3\tScore");
   }
 
   static void printRow(int frame, int roll1, int roll2, int roll3, int score){
-      System.out.printf("%d\t" + "%d\t" + "%d\t" + "%d\t" + "%d", frame,
+      System.out.printf("%d\t" + "%d\t" + "%d\t" + "%d\t" + "%d%n", frame,
               roll1, roll2, roll3, score);
   }
 
